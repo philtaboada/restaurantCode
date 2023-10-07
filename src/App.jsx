3d65af4afb7e87f8f733324efa9ae795027dc58d
philtaboada <philtaboada1997@gmail.com>
2023-10-07 13:30:08 -0500

    templete

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Chef from './container/Chef/Chef';
 import Intro from './container/Intro/Intro';
 import Laurels from './container/Laurels/Laurels';
 import Gallery from './container/Gallery/Gallery';
-import FindUs from './container/FindUs/FindUs';
+import FindUs from './container/Findus/FindUs';
 import Footer from './container/Footer/Footer';
 
 const App = () => (
